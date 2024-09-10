0e60acdcc17fd4f86d519e33483056fe3a78b4c5
sivakumar966 <sivakumar966@gmail.com>
2024-09-10 13:05:37 +0530

    jumpingOnClouds Completed

--- Warm-up/Program.cs
+++ Warm-up/Program.cs
@@ -1,8 +1,10 @@
 ﻿// sockMerchant --------------------------------------------------------------------------------------------------------------
 
+using System.Linq;
+
 int n = 9;
 List<int> ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
-Console.WriteLine(sockMerchant(n, ar));
+Console.WriteLine($"sockMerchant : {sockMerchant(n, ar)}");
 
 int sockMerchant(int n, List<int> ar)
 {
@@ -42,12 +44,43 @@ int countingValleys(int steps, string path)
     return down;
 }
 
-Console.WriteLine(countingValleys(steps, path));
+Console.WriteLine($"countingValleys : {countingValleys(steps, path)}");
+
+// jumpingOnClouds -------------------------------------------------------------------------------------------------------------- 
+
+List<int> clouds1 = [0, 0, 1, 0, 0, 1, 0]; // 4
+List<int> clouds2 = [0, 0, 0, 0, 1, 0]; // 3
+List<int> clouds3 = [0, 0, 0, 0, 1, 0, 1]; // 3
+
+int jumpingOnClouds(List<int> c)
+{
+    int jumpCount = 0;
+    for (int i = 0; i < c.Count - 1;)
+    {
+        if (i + 2 < c.Count && c[i + 2] == 0)
+        {
+            jumpCount++;
+            i += 2;
+        }
+        else if (i + 1 < c.Count && c[i + 1] == 0)
+        {
+            jumpCount++;
+            i++;
+        }
+        else
+        {
+            i++;
+        }
 
 
 
+    }
+    return jumpCount;
+}
 
-
+Console.WriteLine($"jumpingOnClouds - clouds1: {jumpingOnClouds(clouds1)}");
+Console.WriteLine($"jumpingOnClouds - clouds2: {jumpingOnClouds(clouds2)}");
+Console.WriteLine($"jumpingOnClouds - clouds3: {jumpingOnClouds(clouds3)}");
 
 // -------------------------------------------------------------------------------------------------------------- 
 Console.ReadLine();
